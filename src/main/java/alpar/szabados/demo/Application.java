7ff1cc7cfd3db46d6e14c7b47c6bbce401692c05
Szabados Alpár <szabadosalpar@gmail.com>
2017-08-07 14:18:32 +0300

    Added commandLineRunner `Bean` to list the provided beans

--- src/main/java/alpar/szabados/demo/Application.java
+++ src/main/java/alpar/szabados/demo/Application.java
@@ -2,15 +2,34 @@ package alpar.szabados.demo;
 
 import alpar.szabados.demo.dao.UserDao;
 import alpar.szabados.demo.entities.User;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+import java.util.Arrays;
+
+@SpringBootApplication
 public class Application {
-    public static void main(String... args) {
-        UserDao.addNewUser(new User("Anca", "1234"));
-        printUserEntries();
+
+    public static void main(String[] args) {
+        SpringApplication.run(alpar.szabados.demo.Application.class, args);
     }
 
-    private static void printUserEntries() {
-        UserDao.getAllUsers().forEach(System.out::println);
+//    private static void printUserEntries() {
+//        UserDao.getAllUsers().forEach(System.out::println);
+//    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            Arrays.stream(ctx.getBeanDefinitionNames())
+                  .sorted()
+                  .forEach(System.out::println);
+        };
     }
 }
 
