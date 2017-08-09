cf2ea0863bfbd94a748daf60032caeba6e9c99f4
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-09 22:28:39 +0300

    Renamed column-names and removed unnecessary getters and setters

--- src/main/java/alpar/szabados/demo/entities/User.java
+++ src/main/java/alpar/szabados/demo/entities/User.java
@@ -5,51 +5,43 @@ import javax.persistence.*;
 @Entity
 @Table(name = "user")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "userId", nullable = false, unique = true)
-    private long userId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id", nullable = false, unique = true)
+	private long userId;
 
-    @Column(name = "userName")
-    private String userName;
+	@Column(name = "user_name", nullable = false, unique = true)
+	private String userName;
 
-    @Column(name = "password")
-    private String password;
+	@Column(name = "password")
+	private String password;
 
-    public User(String userName, String password) {
-        this.userName = userName;
-        this.password = password;
-    }
+	public User(String userName, String password) {
+		this.userName = userName;
+		this.password = password;
+	}
 
-    public User() {
-    }
+	public User() {
+	}
 
-    public Long getUserId() {
-        return userId;
-    }
+	public String getUserName() {
+		return userName;
+	}
 
-    public void setUserId(Long id) {
-        this.userId = id;
-    }
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-    public String getUserName() {
-        return userName;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setUserName(String user_name) {
-        this.userName = user_name;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("User{userId=%d, userName='%s', password='%s'}", userId, userName, password);
-    }
+	@Override
+	public String toString() {
+		return String.format("User{userId=%d, userName='%s', password='%s'}", userId, userName, password);
+	}
 }
